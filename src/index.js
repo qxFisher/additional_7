3d36ca57746ed34cedebdf442117964542ec43ba
Lesnik Matvey <qxfisher@gmail.com>
2018-10-03 16:14:46 +0300

    Implemented 2 functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  solve(matrix);
+  return matrix;
+}
+
+function solve(matrix) {
+  let i, j, b, k;
+  for (i = 0; i <= 8; i++) {
+    for (j = 0; j <= 8; j++) {
+      if (matrix[i][j] === 0) {
+        for (k = 1; k <= 9; k++) {
+          if (insert(matrix, i, j, k)) {
+            matrix[i][j] = k;
+            b = solve(matrix);
+            if (b) { return true; }
+            matrix[i][j] = 0;
+          }
+        }
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function insert(matrix, i, j, k) {
+  let a, b;
+  for (a = 0; a <= 8; a++) {
+    if (a != i && matrix[a][j] == k) {
+      return false;
+    }
+  }
+  for (a = 0; a <= 8; a++) {
+    if (a != j && matrix[i][a] == k) {
+      return false;
+    }
+  }
+  let y = Math.floor((i / 3)) * 3,
+      x = Math.floor((j / 3)) * 3;
+  for (a = 0; a < 3; a++) {
+    for (b = 0; b < 3; b++) {
+      if (a != i && b != j && matrix[y + a][x + b] == k) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
